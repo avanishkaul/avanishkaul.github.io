3431ddcc10b0b7cec27542f221e14187cad2f409
avanishkaul <57873565+avanishkaul@users.noreply.github.com>
2023-12-12 18:05:16 -0300

    initial commit

---
+++ logic.js
@@ -0,0 +1,18 @@
+
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+
+var downloadButton = document.getElementById('downloadButton');
+var deviceMessage = document.getElementById('deviceMessage');
+
+
+if (isMobile()) {
+
+    deviceMessage.innerHTML = "Estás usando un dispositivo móvil. Puedes descargar la aplicación aqui.";
+} else {
+
+    deviceMessage.innerHTML = "Estás usando un PC. La descarga está disponible";
+}
+
